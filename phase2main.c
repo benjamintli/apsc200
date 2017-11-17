89f184e39b76696e867c53bd68f1a4165434b0ea
benjamintli <15btl1@queensu.ca>
2017-11-17 15:46:28 -0500

    added switch

--- phase2main.c
+++ phase2main.c
@@ -2,5 +2,22 @@
 
 int main()
 {
+	int select;
+	switch (select)
+	{
+	case 0:
+		break;
+	case 1:
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	case 5:
+		break;
+
+	}
 	return 0;
 }
