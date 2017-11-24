bd50bff6398c827b70e9a9eda149ab60e9b2c0b9
benjamintli <15btl1@queensu.ca>
2017-11-24 15:12:49 -0500

    added if statements

--- circuitbreaker.h
+++ circuitbreaker.h
@@ -9,11 +9,7 @@ typedef struct queue {
 }queue;
 
 void removeFront(){
-	struct Node* temp = front;
-	if(front!= NULL){
-		front = front->next;
-	}
-	free(temp);
+	struct Node* temp;
 }
 
 void addEnd();
--- phase2main.c
+++ phase2main.c
@@ -1,10 +1,83 @@
 #include <stdio.h>
 #include <string.h>
+#include "circuitbreaker.h"
+
+#pragma warning (disable : 4996)
 
 int main()
 {
 	char choice[10];
-	scanf('%d', choice);
-	if (strcmp )
+	printf("Make a selection:");
+	scanf('%s', choice);
+	if (strcmp("BLS1", choice) == 0)
+	{
+
+	}
+	else if (strcmp("BLS2", choice) == 0)
+	{
+
+	}
+	else if (strcmp("BLS-TPS", choice) == 0)
+	{
+
+	}
+	else if (strcmp("DC1", choice) == 0)
+	{
+		char state[1];
+		printf("Open or Close? O/C:");
+		scanf("%c", state);
+		if (strcmp("O", choice) == 0)
+		{
+
+		}
+		else if (strcmp("C", choice) == 0)
+		{
+
+		}
+	}
+	else if (strcmp("DC2", choice) == 0)
+	{
+		char state[1];
+		printf("Open or Close? O/C:");
+		scanf("%c", state);
+		if (strcmp("O", choice) == 0)
+		{
+
+		}
+		else if (strcmp("C", choice) == 0)
+		{
+
+		}
+	}
+	else if (strcmp("DC3", choice) == 0)
+	{
+		char state[1];
+		printf("Open or Close? O/C:");
+		scanf("%c", state);
+		if (strcmp("O", choice) == 0)
+		{
+
+		}
+		else if (strcmp("C", choice) == 0)
+		{
+
+		}
+	}
+	else if (strcmp("DC4", choice) == 0)
+	{
+		char state[1];
+		printf("Open or Close? O/C:");
+		scanf("%c", state);
+		if (strcmp("O", choice) == 0)
+		{
+
+		}
+		else if (strcmp("C", choice) == 0)
+		{
+
+		}
+	}
+
+
 	return 0;
 }
