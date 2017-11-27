6866782a7cd66731664dd0f52c697566623ece5d
benjamintli <15btl1@queensu.ca>
2017-11-27 12:33:05 -0500

    made some changes

--- circuitbreaker.h
+++ circuitbreaker.h
@@ -1,18 +1,41 @@
-
-typedef struct switches {
-	char* sName;
-	int state;
-}switches;
-
-typedef struct queue {
-
-}queue;
-
-void removeFront(){
-	struct Node* temp;
-}
-
-void addEnd();
+//
+//#pragma warning (disable : 4996)
+//
+//
+//typedef struct switches {
+//	char* sName;
+//	int state;
+//}switches;
+//
+//typedef struct queue {
+//	int front, end;
+//	switches *s[6];
+//	int size;
+//	int counter;
+//}queue;
+//
+//void removeFront(){
+//	struct Node* temp;
+//}
+//
+//
+//queue* initQueue()
+//{
+//	queue *q = (queue *)malloc(sizeof(queue));
+//	q->front = 0;
+//	q->end = 5;
+//	q->counter = 0;
+//	return q;
+//}
+//
+//void addEnd(queue *q, char* name, char* state)
+//{
+//	switches *s = (switches *)malloc(sizeof(switches));
+//	strcpy (s->sName, name);
+//	strcpy (s->state, state);
+//	q->s[q->counter] = &s;
+//	q->counter++;
+//}
 
 
 
--- phase2main.c
+++ phase2main.c
@@ -1,25 +1,52 @@
 #include <stdio.h>
 #include <string.h>
-#include "circuitbreaker.h"
 
 #pragma warning (disable : 4996)
+
+typedef struct switches {
+	char* sName;
+	int state;
+}switches;
+
+
 
 int main()
 {
 	char choice[10];
+	switches *s = (switches*)malloc(sizeof(switches) * 6);
+	s[0].sName = "DC1";
+	s[1].sName = "DC2";
+	s[2].sName = "DC3";
+	s[3].sName = "DC4";
+	s[4].sName = "ACM";
+	s[5].sName = "DCM";
+
+	s[0].state = 1;
+	s[1].state = 1;
+	s[2].state = 1;
+	s[3].state = 1;
+	s[4].state = 1;
+	s[5].state = 1;
+
+
+
 	printf("Make a selection:");
-	scanf('%s', choice);
+	scanf("%s", &choice);
 	if (strcmp("BLS1", choice) == 0)
 	{
-
+		s[0].state = 0;
+		s[1].state = 0;
+		printf(" %s and %s close", s[0].sName, s[1].sName);
 	}
 	else if (strcmp("BLS2", choice) == 0)
 	{
-
+		s[2].state = 1;
+		s[3].state = 1;
 	}
 	else if (strcmp("BLS-TPS", choice) == 0)
 	{
-
+		s[4].state = 1;
+		s[5].state = 1;
 	}
 	else if (strcmp("DC1", choice) == 0)
 	{
